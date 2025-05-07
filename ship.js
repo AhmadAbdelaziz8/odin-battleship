88ca6e36f40f72b558283cccdef68f24905e6509
iCobra3 <ahmadabdelaziz8@gmail.com>
2025-05-07 16:55:10 +0300

    add the ship component

---
+++ ship.js
@@ -0,0 +1,29 @@
+'''
+// Factory function to create Ship objects
+const Ship = (length) => {
+  if (length <= 0) {
+    throw new Error('Ship length must be positive');
+  }
+  let hitCount = 0;
+
+  const getLength = () => length;
+  const getHits = () => hitCount;
+
+  const hit = () => {
+    hitCount++;
+  };
+
+  const isSunk = () => {
+    return hitCount >= length;
+  };
+
+  return {
+    getLength,
+    getHits,
+    hit,
+    isSunk,
+  };
+};
+
+export default Ship;
+'''
---
+++ ship.test.js
@@ -0,0 +1,49 @@
+'''
+import Ship from './ship';
+
+describe('Ship', () => {
+  let testShip;
+
+  beforeEach(() => {
+    testShip = Ship(3); // Create a ship of length 3 for testing
+  });
+
+  test('should create a ship with the correct length and 0 hits', () => {
+    expect(testShip.getLength()).toBe(3);
+    expect(testShip.getHits()).toBe(0);
+  });
+
+  test('hit() should increment the number of hits', () => {
+    testShip.hit();
+    expect(testShip.getHits()).toBe(1);
+    testShip.hit();
+    expect(testShip.getHits()).toBe(2);
+  });
+
+  test('isSunk() should return false for a new ship', () => {
+    expect(testShip.isSunk()).toBe(false);
+  });
+
+  test('isSunk() should return false for a ship that is hit but not sunk', () => {
+    testShip.hit();
+    testShip.hit();
+    expect(testShip.isSunk()).toBe(false);
+  });
+
+  test('isSunk() should return true when hits equal length', () => {
+    testShip.hit();
+    testShip.hit();
+    testShip.hit();
+    expect(testShip.isSunk()).toBe(true);
+  });
+
+  test('isSunk() should remain true even if hit more than length', () => {
+    testShip.hit();
+    testShip.hit();
+    testShip.hit();
+    testShip.hit(); // Extra hit
+    expect(testShip.isSunk()).toBe(true);
+    expect(testShip.getHits()).toBe(4); // Hits can exceed length
+  });
+});
+'''
